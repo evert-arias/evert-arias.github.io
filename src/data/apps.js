89b6fedcbe2df2114e0fb74b22072f6a79f751a3
Evert Arias <evert@codefor.me>
2020-02-07 15:37:36 -0500

    Add apps section

---
+++ src/data/apps.js
@@ -0,0 +1,19 @@
+module.exports = {
+  enabled: true,
+  apps: [
+    {
+      id: 1,
+      show: true,
+      name: 'Printable',
+      desc: 'A beautiful printable calendar',
+      url: 'https://apps.earias.me/printcal',
+    },
+    {
+      id: 2,
+      show: true,
+      name: 'NowHa',
+      desc: 'The barbershop app that finds the right place and price for you',
+      url: 'https://apps.earias.me/nowha',
+    },
+  ],
+};
