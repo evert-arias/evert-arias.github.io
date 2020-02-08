cd73ade29807a27738a07df0d02c827dc0773434
Evert Arias <evert@codefor.me>
2020-02-08 17:46:17 -0500

    Add github navbar link

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -7,6 +7,7 @@ const NavbarLinks = ({ desktop }) => (
     <AnchorLink href="#apps">Apps</AnchorLink>
     <AnchorLink href="#projects">Projects</AnchorLink>
     <a href="https://blog.earias.me">Blog</a>
+    <a href="https://github.com/evert-arias">Github</a>
     <AnchorLink href="#about">About</AnchorLink>
     <AnchorLink href="#contact">Contact</AnchorLink>
   </Wrapper>
