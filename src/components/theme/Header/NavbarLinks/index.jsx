916bdb3fe44cbd925f715705d7afd5362291d0e2
Evert Arias <evert@codefor.me>
2020-02-08 14:57:41 -0500

    Update

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Wrapper } from './styles';
 
-const NavbarLinks = desktop => (
+const NavbarLinks = ({ desktop }) => (
   <Wrapper desktop={desktop}>
     <AnchorLink href="#apps">Apps</AnchorLink>
     <AnchorLink href="#projects">Projects</AnchorLink>
-    <AnchorLink href="https://blog.earias.me">Blog</AnchorLink>
+    <a href="https://blog.earias.me">Blog</a>
     <AnchorLink href="#about">About</AnchorLink>
     <AnchorLink href="#contact">Contact</AnchorLink>
   </Wrapper>
